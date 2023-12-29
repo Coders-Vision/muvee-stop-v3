02a81cd924f5389a963005d95b0db3c1310a4b14
Coders-Vision <fowzan.alpha@gmail.com>
2023-12-29 16:57:31 +0300

    feat. Implemented Page for upcoming movies

--- src/app/movies/upcoming/page.tsx
+++ src/app/movies/upcoming/page.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import MoviesPaginated from "@/components/movie-paginated";
+import Container from "@/layout/container";
+import { Metadata } from "next";
+import { MovieFilterParams } from "@/types/filter/filter-params";
+import { discoverMovies } from "@/actions/movies/discover-movies";
+import { addDate, getDate,  } from "@/lib/date-funcs";
 
-function Upcoming() {
+type UpcomingMovie = {
+  searchParams: MovieFilterParams;
+};
+
+//Next js SEO Tag Generation
+export const metadata: Metadata = {
+  title: "Upcoming Movies",
+};
+
+// https://api.themoviedb.org/3/discover/movie?page=1&release_date.gte={min_date}&release_date.lte={max_date}
+async function UpcomingMovies({ searchParams }: UpcomingMovie) {
+  const twoMonthsAdd = addDate(60);
+  const fouraMonthsAdd = addDate(240);
+
+  const nowPlayingMovies = await discoverMovies({
+    page: searchParams.page,
+    sort_by: "popularity.desc",
+    with_release_type: "2|3",
+    "release_date.gte": twoMonthsAdd,
+    "release_date.lte": fouraMonthsAdd,
+  });
+
   return (
-    <div>Upcoming</div>
-  )
+    <Container>
+      {twoMonthsAdd} ==- {fouraMonthsAdd}
+      <h1 className="font-semibold text-xl mt-4 mx-4">Upcoming Movies</h1>
+      <MoviesPaginated
+        movies={nowPlayingMovies}
+        paginatePath={"/movies/upcoming"}
+      />
+    </Container>
+  );
 }
 
-export default Upcoming
+export default UpcomingMovies;
--- src/lib/date-funcs.ts
+++ src/lib/date-funcs.ts
@@ -1,6 +1,11 @@
-import { format, startOfDay, subDays } from "date-fns";
+import { addDays, format, startOfDay, subDays } from "date-fns";
 
 export const getDate = () => format(new Date(), "yyyy-MM-dd");
 
+export const addDate= (days: number) =>
+  format(startOfDay(addDays(new Date(), days)), "yyyy-MM-dd");
+
 export const getDifference = (days: number) =>
   format(startOfDay(subDays(new Date(), days)), "yyyy-MM-dd");
+
+
