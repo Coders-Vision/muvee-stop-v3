af0584171e531e34a71669e029a28afcdab3599d
Coders-Vision <fowzan.alpha@gmail.com>
2023-12-29 19:51:38 +0300

    feat. Implemented Top Rated Shows Page

--- src/app/shows/top-rated/page.tsx
+++ src/app/shows/top-rated/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { discoverShows } from "@/actions/shows/discover-shows";
+import Container from "@/layout/container";
+import { ShowFilterParams } from "@/types/filter/filter-params";
+import { Metadata } from "next";
+import ShowsPaginated from "@/components/show-paginated";
 
-function TopRated() {
+type TopRatedShow = {
+  searchParams: ShowFilterParams;
+};
+
+//Next js SEO Tag Generation
+export const metadata: Metadata = {
+  title: "Top Rated Shows",
+};
+
+
+async function TopRated({ searchParams }: TopRatedShow) {
+  const topRatedShows = await discoverShows({
+    page: searchParams.page,
+    sort_by: "vote_average.desc",
+    "vote_count.gte":"200"
+  });
+
   return (
-    <div>TopRated</div>
-  )
+    <Container>
+      <h1 className="font-semibold text-xl mt-4 mx-4">Top Rated Shows</h1>
+      <ShowsPaginated
+        shows={topRatedShows}
+        paginatePath={"/shows/top-rated"}
+      />
+    </Container>
+  );
 }
 
-export default TopRated
+export default TopRated;
