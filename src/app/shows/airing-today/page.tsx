b223977ec1fb5704646d2e1106af7a2fa1f286de
Coders-Vision <fowzan.alpha@gmail.com>
2023-12-29 19:34:10 +0300

    feat. Implemented Shows Airing Today Page

--- src/app/shows/airing-today/page.tsx
+++ src/app/shows/airing-today/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { discoverShows } from "@/actions/shows/discover-shows";
+import Container from "@/layout/container";
+import { ShowFilterParams } from "@/types/filter/filter-params";
+import { Metadata } from "next";
+import ShowsPaginated from "@/components/show-paginated";
+import { getDate, getDifference } from "@/lib/date-funcs";
 
-function AiringToday() {
+type AiringTodayShow = {
+  searchParams: ShowFilterParams;
+};
+
+//Next js SEO Tag Generation
+export const metadata: Metadata = {
+  title: "Shows Airing Today",
+};
+
+async function AiringToday({ searchParams }: AiringTodayShow) {
+  const getTodaysDate = getDate();
+  const ondeDayDiff = getDifference(1);
+
+  const showsAiringToday = await discoverShows({
+    page: searchParams.page,
+    sort_by: "popularity.desc",
+    "air_date.gte": getTodaysDate,
+    "air_date.lte": getTodaysDate,
+  });
+
   return (
-    <div>AiringToday</div>
-  )
+    <Container>
+      <h1 className="font-semibold text-xl mt-4 mx-4">Shows Airing Today</h1>
+      <ShowsPaginated
+        shows={showsAiringToday}
+        paginatePath={"/shows/airing-today"}
+      />
+    </Container>
+  );
 }
 
-export default AiringToday
+export default AiringToday;
