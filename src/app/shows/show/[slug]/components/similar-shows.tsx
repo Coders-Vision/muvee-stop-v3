be06b99f55d93c21ca3e48d6391cbb2196f6a027
Coders-Vision <fowzan.alpha@gmail.com>
2025-03-22 23:34:13 +0300

    chore: Adjusted Similar Content for both Movies and Shows

--- src/app/shows/show/[slug]/components/similar-shows.tsx
+++ src/app/shows/show/[slug]/components/similar-shows.tsx
@@ -1,10 +1,12 @@
+// "use client"
+// import { getSimilarShows } from "@/actions/shows/get-similar-shows";
 import ShowSlider from "@/components/show-slider";
 import { Result } from "@/types/show/show-result";
-// import { getSimilarShows } from "@/actions/shows/get-similar-shows";
+
 
 // async function SimilarShows({ showId }: { showId: number }) {
 //   const similarShows = await getSimilarShows(showId);
-async function SimilarShows({ results }: { results: Result[] }) {
+function SimilarShows({ results }: { results: Result[] }) {
   return (
     <ShowSlider
       title="Similar Shows"
