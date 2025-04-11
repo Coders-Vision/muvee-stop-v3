273560a29f782ea00e500d14d28c69b6ec904cb9
Coders-Vision <fowzan.alpha@gmail.com>
2025-04-11 13:59:52 +0300

    fix: Fixed Link component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,22 +1,24 @@
+import Link from "next/link";
+
 function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center ">
       <div className="flex flex-col items-center justify-center my-28 px-10">
-          <div className="text-center">
-            <h4 className="text-3xl font-extralight">Not Found</h4>
-            <h1 className="text-9xl font-thin">404</h1>
-            <p className="text-xl font-mono">
-              Oh No! Seems like this page does not exist!
-            </p>
-          </div>
+        <div className="text-center">
+          <h4 className="text-3xl font-extralight">Not Found</h4>
+          <h1 className="text-9xl font-thin">404</h1>
+          <p className="text-xl font-mono">
+            Oh No! Seems like this page does not exist!
+          </p>
         </div>
-      <a
+      </div>
+      <Link
         href="/"
         className="flex item-center gap-x-2 bg-transparent border-2 border-white rounded-full text-white px-4 py-2
       "
       >
         Go Back Home
-      </a>
+      </Link>
     </div>
   );
 }
